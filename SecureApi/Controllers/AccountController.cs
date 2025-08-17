09c8c5fd84c0d448f0fb2c77ff6d83560438dee8
dabananda <dabananda.dev@gmail.com>
2025-08-17 15:18:12 +0600

    feat: user register with email and password

---
+++ SecureApi/Controllers/AccountController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SecureApi.Models;
+using SecureApi.Models.DTOs;
+
+namespace SecureApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterDto model)
+        {
+            if (model == null || !ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var user = new ApplicationUser
+            {
+                UserName = model.Email,
+                Email = model.Email
+            };
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+                return Ok(new { message = "User registered successfully." });
+            }
+            return BadRequest(result.Errors);
+        }
+    }
+}
